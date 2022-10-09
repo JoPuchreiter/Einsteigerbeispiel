cef30ba5fcf4ad59daf74bec9f0b9cf7d44c5825
JoPuchreiter <jopuchreiter@aau.edu.at>
2022-10-09 18:19:50 +0200

    Implementation getRepetitions

---
+++ src/main/java/Einsteigerbeispiel.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+
+public class Einsteigerbeispiel {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 1, 1, 1, 1, 2, 3, 3, 4, 4, 4, 4, 5, 5, 6, 6, 6, 7, 8, 8, 9};
+
+        System.out.println(getRepetitions(array));
+
+    }
+
+    public static HashMap<Integer, Integer> getRepetitions(int[] array) {
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        int a=1;
+
+
+        for (int i = 1; i < array.length; i++) {
+
+
+                if (array[i] == array[i-1]) {
+                    a++;
+
+                }
+
+            hashMap.put(array[i],a);
+
+                if (array[i] != array[i-1]){
+                    a=1;
+                }
+
+            hashMap.put(array[i],a);
+
+
+
+        }
+
+        return hashMap;
+
+    }
+}
